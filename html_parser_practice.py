119ad0e8cc047b7cdbc15d882a59985bab7a7970
tarekrahman3 <44648395+tarekrahman3@users.noreply.github.com>
2021-01-04 22:27:11 +0600

    Update html_parser_practice.py

--- html_parser_practice.py
+++ html_parser_practice.py
@@ -12,22 +12,15 @@ options = Options()
 driver = webdriver.Chrome(options=options, executable_path='chromedriver')
 driver.get("http://www.google.com")  
 time.sleep(3)
+
 que=driver.find_element_by_xpath("//input[@name='q']")
-que.send_keys('Beautifulsoup Parse Tutorial')
+que.send_keys('site:reddit.com bs4 tutorial')
 time.sleep(3)
 que.send_keys(Keys.RETURN)
 time.sleep(4)
-a=driver.page_source
-name = 'a1.txt'
-try:
-    file = open(name,'w',encoding='utf-8')
-    file.write(a)
-    file.close()
 
-except:
-     print('page'+str(x)+'\n')
-       
-a=""
-time.sleep(3)
-   
-#driver.close()
+html=driver.page_source
+soup=BeautifulSoup(html, "lxml")
+
+for a in soup.find_all('a', href=True):
+    print ("URL:", a['href'])
