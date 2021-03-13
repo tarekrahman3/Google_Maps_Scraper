d792b4b2b2df66d89c957050477d46874076cbb7
tarekrahman3 <44648395+tarekrahman3@users.noreply.github.com>
2021-03-13 18:51:11 +0600

    Update and rename html_parser_practice.py to url_generator.py

---
+++ url_generator.py
@@ -0,0 +1,71 @@
+regions = (
+'Tupelo',
+'Meridian',
+'Olive Branch',
+'Greenville'
+)
+keywords = (
+'Dog Boarding OR Dog Daycare OR Dog Overnight OR Pet Boarding OR Pet Daycare',
+'Dog Boarding',
+'Dog Daycare'
+)
+from selenium import webdriver
+import time
+import os
+import pandas as pd
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+options = Options()
+options.add_argument("--no-sandbox")
+options.add_experimental_option("useAutomationExtension", False)
+options.add_experimental_option("excludeSwitches",["enable-automation"])
+driver=webdriver.Chrome(options=options, executable_path='/home/tarek/MY_PROJECTS/Selenium_Projects/webdrivers/chromedriver')
+
+col1 = []
+col2 = []
+col3 = []
+
+def search_region(region):
+	driver.get("https://www.google.com/maps" + "?hl=en")
+	WebDriverWait(driver, 60).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="searchboxinput"]')))
+	search_box = driver.find_element_by_xpath('//*[@id="searchboxinput"]')
+	search_box.click()
+	search_box.send_keys(str(region) + 'MS')
+	WebDriverWait(driver, 60).until(EC.presence_of_element_located((By.XPATH, '//*[@id="suggestions-grid"]/div[1]')))
+	evaluate_first_result = driver.find_element_by_xpath('//*[@id="suggestions-grid"]/div[1]').text
+	search_box.send_keys(Keys.DOWN)
+	search_box.send_keys(Keys.ENTER)
+	location_url = driver.current_url
+	WebDriverWait(driver, 60).until(EC.element_to_be_clickable((By.XPATH, '//button[contains(@aria-label, "Search nearby")]')))
+	search_nearby_button = driver.find_element_by_xpath('//button[contains(@aria-label, "Search nearby")]')
+	search_nearby_button.click()
+
+ def search_by_keyword(key):
+	WebDriverWait(driver, 60).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="searchboxinput"]')))
+	business_search_box = driver.find_element_by_xpath('//*[@id="searchboxinput"]')
+	business_search_box.click()
+	business_search_box.clear()
+	business_search_box.send_keys(key)
+	business_search_box.send_keys(Keys.ENTER)
+	time.sleep(5)
+	generated_url = driver.current_url
+	print(generated_url)
+	col1.append(f"{str(each_city)}")
+	col2.append(f"{str(key)}")
+	col3.append(generated_url)
+	
+
+for each_city in regions:
+	search_region(each_city)
+	for each_keyword in keywords:
+		search_by_keyword(each_keyword)
+driver.quit()
+data = {'city': col1,
+'keyword': col2,
+'generated_url':col3
+}
+file_name = str(ctime()) + ' Pet Boarding Maps URL.csv'
+data_frame = pd.DataFrame(data, columns = ['city', 'keyword', 'generated_url']).to_csv(file_name, index=None, header=True)
