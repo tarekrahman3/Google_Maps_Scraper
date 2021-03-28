3c44a8331e02000325c06b663ad30144cbccf6e4
Tarek R <44648395+tarekrahman3@users.noreply.github.com>
2021-03-29 00:12:59 +0600

    Update map_scraper (pushed into bash script).py

--- map_scraper (pushed into bash script).py
+++ map_scraper (pushed into bash script).py
@@ -18,7 +18,7 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-
+from selenium.webdriver.common.action_chains import ActionChains
 options = Options()
 options.add_argument("--no-sandbox")
 options.add_experimental_option("useAutomationExtension", False)
@@ -39,50 +39,54 @@ col11 = []
 
 def headers_loop():
 	try:
-		WebDriverWait(driver, 15).until(EC.element_to_be_clickable((By.XPATH, "//h3")))
+		WebDriverWait(driver, 15).until(EC.element_to_be_clickable((By.XPATH, "//div[contains(@jsaction, 'mouseover:pane')]/a")))
 	except:
+		pass
+	for i in range(20):
 		try:
-			WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, '//div[@class="section-result-content"]')))
+			WebDriverWait(driver, 15).until(EC.element_to_be_clickable((By.XPATH, "//div[contains(@jsaction, 'mouseover:pane')]/a")))
 		except:
-			time.sleep(10)
-	results= driver.find_elements_by_xpath("//h3")
-	i=0
-	for i in range(len(results)):
-		try:
-			WebDriverWait(driver, 15).until(EC.element_to_be_clickable((By.CLASS_NAME, "section-result-content")))
-		except:
+			pass
+		while True:
 			try:
-				WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, '//div[@class="section-result-content"]')))
+				r = driver.find_elements_by_xpath("//div[contains(@aria-label, 'Results')]/div//a[contains(@href, 'http')]")
+				action = ActionChains(driver)
+				r_len = (len(r))-1
+				action.move_to_element(r[r_len])
+				action.perform()
+				r[r_len].location_once_scrolled_into_view		
+				if (len(r))==20:
+					break
+					print('scroll done')
+				else:
+					pass
 			except:
-				time.sleep(10)
+				break
+		results = driver.find_elements_by_xpath("//div[contains(@jsaction, 'mouseover:pane')]")
+		#//div[contains(@aria-label, "Results")]/div[contains(@jstcache, "251")]
+	
 		c_time = ctime()
-		results = driver.find_elements_by_xpath("//h3")
 		try:
 			rate = results[i].find_element_by_xpath(".//span[contains(@class, 'rating-score')]").text
 		except:
 			rate = ''
 		try:
-			ratings = results[i].find_element_by_xpath(".//span[contains(@class, 'num-ratings')]").text
+			ratings = results[i].find_element_by_xpath(".//div[contains(@class, 'rating-container')]/span[2]//span[@role='img']").get_attribute('aria-label')
 		except:
 			ratings = ''
 		try:
-			details = results[i].find_element_by_xpath(".//span[contains(@class, 'result-details')]").text
+			details = results[i].find_element_by_xpath(".//div[contains(@class, 'info-line')]/span/jsl").text
 		except:
 			details = ''
 		try:
 			location=results[i].find_element_by_xpath(".//span[contains(@class, 'result-location')]").text
 		except:
 			location = ''
+
+		
+		'''
 		try:
-			phones = results[i].find_element_by_xpath(".//span[contains(@class, 'phone-number')]").text
-		except:
-			phones = ''
-		try:
-			website = results[i].find_element_by_xpath(".//div[contains(@class, 'result-action-container')]//a").get_attribute('href')
-		except:
-			website = ''
-		try:
-			WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "//h3")))
+			WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, ".//h3")))
 		except:
 			time.sleep(5)
 		try:
@@ -97,6 +101,13 @@ def headers_loop():
 				results[i].click()
 			except:
 				results[i].click()
+		'''
+
+		r = driver.find_elements_by_xpath("//div[contains(@jsaction, 'mouseover:pane')]/a")
+		#.location_once_scrolled_into_view.send_keys(keys.ENTER)
+		action = ActionChains(driver)
+		action.move_to_element(r[i]).click().perform()	
+		
 		wait_for_title = WebDriverWait(driver, 200).until(EC.presence_of_element_located((By.XPATH, '//h1[contains(@class, "section-hero-header-title")]')))
 		business_url = f"{c_time} | {str(driver.current_url)}"
 		title = driver.find_element_by_xpath(".//h1[contains(@class, 'section-hero-header')]").text
@@ -104,6 +115,14 @@ def headers_loop():
 			address = driver.find_element_by_xpath('//button[contains(@data-item-id, "address")]').get_attribute('aria-label')
 		except:
 			address = ''
+		try:
+			website = driver.find_element_by_xpath("//button[contains(@aria-label, 'Website:')]").get_attribute('aria-label')
+		except:
+			website = ''
+		try:
+			phones = driver.find_element_by_xpath("//button[contains(@aria-label, 'Phone:')]").get_attribute('aria-label')
+		except:
+			phones = ''
 		print(f" >  {str((len(col1)+1))}   -    '{title}'")
 		print(f"               {address}")
 		col1.append(business_url)
@@ -117,10 +136,17 @@ def headers_loop():
 		col9.append(address)
 		col10.append(city)
 		col11.append(keyword)
-		back_to_list=driver.find_element_by_xpath('//button[@class="section-back-to-list-button blue-link noprint"]')
+		try:
+			back_to_list=driver.find_element_by_xpath('//button[contains(@class, "section-back")]')
+		except:
+			back_to_list=driver.find_element_by_xpath('//button[@class="searchbox-button"]')
+		
 		back_to_list.click()
-		WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.XPATH, "//h3[contains(@class, 'result-title')]")))
-		WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH, "//h3[contains(@class, 'result-title')]")))
+		time.sleep(1)
+		try:
+			WebDriverWait(driver, 15).until(EC.element_to_be_clickable((By.XPATH, "//div[contains(@jsaction, 'mouseover:pane')]")))
+		except:
+			pass
 
 def next_pagination():
 	try:
@@ -151,16 +177,14 @@ def data_frame():
 
 driver=webdriver.Chrome(options=options, executable_path='/home/tarek/MY_PROJECTS/Selenium_Projects/webdrivers/chromedriver')
 driver.get(str(URL) + '?hl=en')
-while len(col1)<200:
-	try:
-		headers_loop()
-	except:
-		pass
-	try:
-		next_pagination()
-		time.sleep(2)
-	except:
+
+while True:
+	headers_loop()
+	next_pagination()
+	if len(col1)>199:
 		break
+	else:
+		pass
 data_frame()
 driver.quit()
 print('############  Sequence Completed  ############')
