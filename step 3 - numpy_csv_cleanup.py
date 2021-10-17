9340ca1445c71e9ff2e6e9229ce147b8965382f6
Tarek R <44648395+tarekrahman3@users.noreply.github.com>
2021-10-17 21:12:06 +0600

    Delete step 3 - numpy_csv_cleanup.py

--- step 3 - numpy_csv_cleanup.py
+++
@@ -1,6 +0,0 @@
-import glob
-import pandas as pd
-df = pd.concat(map(pd.read_csv, glob.glob('*.csv')))
-#df = map(pd.read_csv, glob.glob('*.csv'))
-n = df.replace({'business_url':r'.+ \| '}, {'business_url':''}, regex=True)
-n.to_csv('a.csv', index = False)
