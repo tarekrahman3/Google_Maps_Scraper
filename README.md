bbb393a164797aa79e20d6f0446bcc3dd29c0381
Tarek R <md.tarek420@gmail.com>
2022-07-15 17:53:04 +0600

    update

--- README.md
+++ README.md
@@ -1,5 +1,10 @@
 # Google Maps Scraper
 Last Updated: Jul 15, 2022
+
+1. Run map_scrape.py
+2. give the google maps search link you want to scrap
+3. Provide the Output File Name.
+
 Video Demonstration
 
 [![Video Demo](https://img.youtube.com/vi/E5TUek362fc/0.jpg)](https://www.youtube.com/watch?v=E5TUek362fc)
--- map_scrape.py
+++ map_scrape.py
@@ -6,12 +6,13 @@ import pandas as pd
 from datetime import datetime
 
 from webdriver_manager.chrome import ChromeDriverManager
+
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-
+from selenium.common.exceptions import TimeoutException
 
 URL = input("input url:")
 Export_File_Name = input("Export_File_Name:")
@@ -49,17 +50,37 @@ def headers_loop():
             '//div[@role="main"]/div/div/div[@data-js-log-root and not(@class)]//a',
         )
         driver.execute_script("arguments[0].scrollIntoView()", elements[i])
-        time.sleep(0.5)
+        time.sleep(1)
+        elements = driver.find_elements(
+            By.XPATH,
+            '//div[@role="main"]/div/div/div[@data-js-log-root and not(@class)]//a',
+        )
         driver.execute_script("arguments[0].click()", elements[i])
-
-        WebDriverWait(driver, 30).until(
-            EC.presence_of_element_located(
-                (
-                    By.XPATH,
-                    parent_box_xpath,
+        try:
+            WebDriverWait(driver, 15).until(
+                EC.element_to_be_clickable(
+                    (
+                        By.XPATH,
+                        parent_box_xpath + "/..//span[*[@viewBox]]",
+                    )
                 )
             )
-        )
+        except TimeoutException:
+            driver.execute_script(
+                "arguments[0].click()",
+                driver.find_elements(
+                    By.XPATH,
+                    '//div[@role="main"]/div/div/div[@data-js-log-root and not(@class)]//a',
+                )[i],
+            )
+            WebDriverWait(driver, 15).until(
+                EC.element_to_be_clickable(
+                    (
+                        By.XPATH,
+                        parent_box_xpath + "/..//span[*[@viewBox]]",
+                    )
+                )
+            )
 
         business_url = f"{str(driver.current_url)}"
 
@@ -109,7 +130,13 @@ def headers_loop():
             ).get_attribute("aria-label")
         except:
             open_days = None
-
+        try:
+            features = driver.find_element(
+                By.XPATH,
+                '//div[@role="region"]/button[contains(@jsaction,"pane.attributes")]',
+            ).text
+        except:
+            features = None
         dict_array.append(
             {
                 "business_url": business_url,
@@ -120,6 +147,7 @@ def headers_loop():
                 "phones": phones,
                 "website": website,
                 "address": address,
+                "features": features,
             }
         )
         print(i, dict_array[-1])
@@ -132,7 +160,11 @@ def headers_loop():
             "arguments[0].click()",
             driver.find_element(By.XPATH, parent_box_xpath + "/..//span[*[@viewBox]]"),
         )
-        time.sleep(2)
+        WebDriverWait(driver, 10).until(
+            EC.invisibility_of_element(
+                (By.XPATH, parent_box_xpath + "/..//span[*[@viewBox]]")
+            )
+        )
         i += 1
 
 
